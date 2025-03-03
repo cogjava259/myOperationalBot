8b1794c1f67a0e7ce75484331be612da0d7f0865
cogjava259 <104674670+cogjava259@users.noreply.github.com>
2025-03-03 21:55:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Data Analysis Assistant",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Configure Streamlit to listen on all interfaces
+import os
+os.environ['STREAMLIT_SERVER_ADDRESS'] = '0.0.0.0'
+os.environ['STREAMLIT_SERVER_PORT'] = '8501'
 
 st.header('Welcome to Streamlit!')
